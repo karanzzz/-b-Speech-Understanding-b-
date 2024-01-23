14ddc83c97aeaa738a73bcca0961f93f4d600474
karanzzz <140894355+karanzzz@users.noreply.github.com>
2024-01-23 13:14:24 +0900

    Update homework11.py

--- 2023_fall/lec11/homework11.py
+++ 2023_fall/lec11/homework11.py
@@ -13,4 +13,21 @@ def transcribe_wavefile(filename, language='en'):
     @returns:
     text (str) - the recognized speech
     '''
-    raise RuntimeError("FAIL!!  You need to change this function so it works!")
+    recognizer = sr.Recognizer()
+
+    with sr.AudioFile(filename) as source:
+        audio_data = recognizer.record(source)
+    
+    try:
+        text = recognizer.recognize_google(audio_data, language=language)
+        return text
+    except sr.UnknownValueError:
+        print("Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {e}")
+
+
+importlib.reload(homework11)
+
+text = homework11.transcribe_wavefile("264752__copyc4t__phone-messages-english-and-italian.flac")
+print(text)
